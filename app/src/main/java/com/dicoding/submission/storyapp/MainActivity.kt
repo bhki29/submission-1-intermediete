18e20752625de9569c0727b8a6534c2318b5aafa
bhki29 <ajiahmadbaehaki20@gmail.com>
2024-12-06 02:13:04 +0700

    adding logic gallery and camera

--- app/src/main/java/com/dicoding/submission/storyapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/submission/storyapp/MainActivity.kt
@@ -3,19 +3,20 @@ package com.dicoding.submission.storyapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.submission.storyapp.databinding.ActivityMainBinding
 import com.dicoding.submission.storyapp.ui.addstory.AddStoryActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.fab.setOnClickListener {
+        val fabAddStory: FloatingActionButton = findViewById(R.id.fab_add)
+
+        fabAddStory.setOnClickListener {
+
             val intent = Intent(this, AddStoryActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/dicoding/submission/storyapp/ui/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/dicoding/submission/storyapp/ui/addstory/AddStoryActivity.kt
@@ -1,21 +1,70 @@
 package com.dicoding.submission.storyapp.ui.addstory
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.submission.storyapp.R
+import com.dicoding.submission.storyapp.costumview.getImageUri
+import com.dicoding.submission.storyapp.databinding.ActivityAddStoryBinding
 
 class AddStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddStoryBinding
+
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_story)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
         }
     }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri!!)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        } else {
+            currentImageUri = null
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.ivPreview.setImageURI(it)
+        }
+    }
+
+    private fun uploadImage() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
 }
